5cd7ca21f21e4531a6fafc8c0ffb421fa79bde5d
Alexander Merkulov <sasha@merqlove.ru>
2018-08-20 23:11:03 +0300

    Restore markdown_helper

--- app/helpers/spree/admin/spree_essentials_helper.rb
+++ app/helpers/spree/admin/spree_essentials_helper.rb
@@ -11,6 +11,15 @@ module Spree
         end
       end
 
+      def markdown_helper
+        content_tag('em', :class => 'small markdown-helper') do
+          [
+              t('essentials.parsed_with'),
+              link_to("Markdown", "http://daringfireball.net/projects/markdown/basics", :onclick => 'window.open(this.href); return false')
+          ].join(" ").html_safe
+        end
+      end
+
       private
 
       def extension_routes
