9944744d4c4d8160b0a84c394601b6885fdf15fb
Alexander Merkulov <sasha@merqlove.ru>
2018-08-26 19:44:41 +0300

    Fix menu overrides

--- app/overrides/admin/spree_essentials.rb
+++ app/overrides/admin/spree_essentials.rb
@@ -1,13 +1,6 @@
 # Adds the Content button to the admin tabs
-Deface::Override.new(:virtual_path  => "spree/layouts/admin",
-                     :name          => "spree_essential_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :partial       => "spree/admin/shared/contents_tab",
-                     :disabled      => false)
-
-# Adds the contents menu. Inside the contents sub menu we ensure it's needed
-Deface::Override.new(:virtual_path  => "spree/layouts/admin",
-                     :name          => "spree_essential_contents_menu",
-                     :insert_top    => "#sub-menu[data-hook]",
-                     :partial       => "spree/admin/shared/contents_sub_menu",
-                     :disabled      => false)
+Deface::Override.new(
+  :virtual_path  => 'spree/layouts/admin',
+  :name          => 'spree_essential_admin_tabs',
+  :insert_after  => 'erb[loud]:contains("render partial: \'spree/admin/shared/main_menu\'")',
+  :partial       => 'admin/shared/contents_tab')
