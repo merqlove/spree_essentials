a58c4e270a72ac69088cd1c58229b890c3589f5f
Alexander Merkulov <sasha@merqlove.ru>
2018-08-29 15:34:33 +0300

    Fix sorting

--- app/controllers/spree/admin/uploads_controller.rb
+++ app/controllers/spree/admin/uploads_controller.rb
@@ -8,7 +8,7 @@ class Spree::Admin::UploadsController < Spree::Admin::ResourceController
 
   def collection
     params[:q] ||= {}
-    params[:q][:sort] ||= "created_at.desc"
+    params[:q][:s] ||= "created_at desc"
     @search = Spree::Upload.search(params[:q])
     @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_orders_per_page])
   end
