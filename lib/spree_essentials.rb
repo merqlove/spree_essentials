b77d4f14d36feec0e517f85813dcd2670b74f70c
Alexander Merkulov <sasha@merqlove.ru>
2018-09-12 14:28:16 +0300

    Fix html_wrap

--- lib/spree_essentials.rb
+++ lib/spree_essentials.rb
@@ -4,24 +4,24 @@ require "spree_essentials/version"
 require "spree_essentials/engine"
 
 module SpreeEssentials
-  
+
   class << self
-  
+
     # Stores an essential-aware extension for use later
     def register(key, extension)
       essentials[key] = extension
     end
-        
+
     # Looks up an extension name 
     def has?(essential)
       essentials.keys.include?(essential.to_sym)
     end
-    
+
     # Returns the array of essential-aware extensions
     def essentials
       @essentials ||= {}
     end
-    
+
   end
-  
+
 end
